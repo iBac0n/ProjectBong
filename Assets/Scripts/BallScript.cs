3fa2d4b85372fdabbab173e1bf5b705aa2f4e2f5
paddog <paddog123@googlemail.com>
2016-11-15 14:47:06 +0100

    Main Push

---
+++ Assets/Scripts/BallScript.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallScript : MonoBehaviour {
+    public float ballSpeed;
+    public float ballBounciness;
+    public Color ballColor;
+
+    private Rigidbody2D rigid2D;
+    private CircleCollider2D circleColl2D;
+    void Start() {
+        rigid2D = this.GetComponent<Rigidbody2D>();
+        circleColl2D = this.GetComponent<CircleCollider2D>();
+
+        StartGame(GetRandomPlayer());
+    }
+
+
+    void Update() {
+
+    }
+
+
+    public GameObject GetRandomPlayer() {
+        GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
+        GameObject selectedPlayer = players[Random.Range(0, players.Length)];
+        Debug.LogError(selectedPlayer);
+        return selectedPlayer;
+    }
+
+    public bool StartGame(GameObject startPlayer) {
+        if(startPlayer == null) {
+            return false;
+        } else {
+            Vector2 velocity = new Vector2(startPlayer.transform.position.x, startPlayer.transform.position.y);
+            velocity.Normalize();
+            rigid2D.AddForce(velocity * ballSpeed);
+            Debug.LogError("Starting!");
+            return true;
+        }
+    }
+    float hitFactor(Vector2 ballPos, Vector2 racketPos,
+                float racketHeight) {
+        // ascii art:
+        // ||  1 <- at the top of the racket
+        // ||
+        // ||  0 <- at the middle of the racket
+        // ||
+        // || -1 <- at the bottom of the racket
+        return (ballPos.y - racketPos.y) / racketHeight;
+    }
+
+    void OnCollisionEnter2D(Collision2D col) {
+        Debug.LogError("COL");
+        if(col.transform.tag == "Player") {
+            Debug.LogError("COLLIDING!");
+            rigid2D.velocity += col.gameObject.GetComponent<Rigidbody2D>().velocity.normalized;
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    public string playerName = "Default";
+    public string playerAxis = "Horizontal";
+    public bool isVerticalPlayer = false;
+    public Color playerColor;
+    public float playerSpeed;
+
+    public int playerID = 0;
+
+
+    private SpriteRenderer sRenderer;
+    void Start() {
+        sRenderer = this.GetComponent<SpriteRenderer>();
+        sRenderer.color = playerColor;
+        this.transform.name = playerName;
+    }
+
+
+    void Update() {
+        GetMovement();
+    }
+
+    void GetMovement() {
+        float _x = 0;
+        float _y = 0;
+        if(!isVerticalPlayer) {
+            _x = Input.GetAxisRaw(playerAxis);
+        }
+        if(isVerticalPlayer) {
+            _y = Input.GetAxisRaw(playerAxis);
+        }
+
+        Vector2 velocity = new Vector3(_x, _y).normalized;
+
+        this.GetComponent<Rigidbody2D>().velocity = velocity * playerSpeed;
+    }
+}
