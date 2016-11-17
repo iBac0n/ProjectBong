b157c88bb0aa1ce011972a1d89d768cf0a8a07c6
paddog <paddog123@googlemail.com>
2016-11-17 17:25:47 +0100

    Basic Popup Spawner
    No effect yet, coming soon TM

---
+++ Assets/Scripts/PopupManager.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public enum PopupTypes {
+    Gravity,
+    Speed
+}
+
+public class PopupManager : MonoBehaviour {
+    public GameObject popupPrefab;
+    public GameObject currentBall;
+    public float cooldown;
+    public List<Popups> currentPopups = new List<Popups>();
+
+    private PopupTypes currentTypeToSpawn;
+    private float timeMark = 0f;
+    void Start() {
+
+    }
+
+
+    void Update() {
+        if(Time.time > timeMark) {
+            Debug.LogError("Trying to spawn a popup!");
+            SpawnPopup(Random.Range(3, 5), currentTypeToSpawn, new Vector3(Random.Range(-4, 4), Random.Range(-4, 4), 0));
+            timeMark = Time.time + cooldown;
+        }
+    }
+
+    public void ActivateEffect(PopupTypes type) {
+        Debug.LogError("Activating");
+    }
+
+    public bool SpawnPopup(float duration, PopupTypes type, Vector3 spawnPosition) {
+        GameObject popupGO = (GameObject)GameObject.Instantiate(popupPrefab, spawnPosition, Quaternion.identity);
+        if(popupGO == null) {
+            Debug.LogError("Failed to spawn Popup");
+            return false;
+        }
+        Popups popupScript = popupGO.GetComponent<Popups>();
+        popupScript.pDuration = duration;
+        popupScript.pManager = this;
+        popupScript.pType = type;
+        return true;
+    }
+}
---
+++ Assets/Scripts/Popups.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(CircleCollider2D))]
+public class Popups : MonoBehaviour {
+
+    public PopupTypes pType;
+    public PopupManager pManager;
+
+    public float pDuration;
+    private bool pSpawned = false;
+    private float timeMark = 0f; 
+    void Start() {
+        pSpawned = true;
+        timeMark = Time.time + pDuration;
+    }
+
+    void Update() {
+        if(pSpawned) {
+            if(Time.time > timeMark) {
+                GameObject.Destroy(this.gameObject);
+            }
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D col) {
+        if(col.transform.tag == "Ball") {
+            pManager.ActivateEffect(pType);
+            GameObject.Destroy(this.gameObject);
+        }
+    }
+}
